149ac76f334864d34ef23da96c933d8ebd5017c5
sainimu78 <393584307@qq.com>
2025-02-24 00:53:39 +0800

    移除测试专门目录, 复用 Project 目录, 简化流程

--- CMakeInclude/InitDefaultRootPaths.cmake
+++ CMakeInclude/InitDefaultRootPaths.cmake
@@ -1,6 +1,4 @@
 set(c_RootGeneratedDirPath ${CMAKE_CURRENT_BINARY_DIR}/Generated)
 set(c_RootSourceDirPath ${c_RootDirPath}/Source)
 set(c_RootProjectDirPath ${c_RootDirPath}/Project)
-set(c_RootTestDirPath ${c_RootDirPath}/Test)
 set(c_RootThirdPartyDirPath ${c_RootDirPath}/ThirdParty)
-get_filename_component(c_RootCMakeProjectFrameworkThirdPartyDirPath "${c_RootCMakeProjectFrameworkDirPath}/../ThirdParty" ABSOLUTE)
