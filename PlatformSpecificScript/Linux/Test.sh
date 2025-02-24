68d59a6c1465eb17faaa5fa52942fcf58c4690cb
sainimu78 <393584307@qq.com>
2025-02-24 21:02:26 +0800

    改用一般 Project 结构, 拆分流程

--- PlatformSpecificScript/Linux/Test.sh
+++ PlatformSpecificScript/Linux/Test.sh
@@ -1,55 +1,19 @@
 #!/bin/bash
-BuildDirPathDebug=$(pwd)/TestBuild/Debug
-BuildDirPathRelease=$(pwd)/TestBuild/Release
+BuildDirPathDebug=$(pwd)/DefaultBuild/Debug
+BuildDirPathRelease=$(pwd)/DefaultBuild/Release
 Toolset="Unix Makefiles"
 OldDirPath=$(pwd)
 
 mkdir -p $BuildDirPathDebug
 cd $BuildDirPathDebug
-GenerateOk=0
-cmake $TestDirPath -G "$Toolset" -DCMAKE_BUILD_TYPE=Debug
+ctest . --output-on-failure -j 1024
 if [ $? -ne 0 ]; then
-    echo "CMake configuration failed for Debug build."
+	echo "CMake test failed for Debug build."
 else
-	mkdir -p $BuildDirPathRelease
 	cd $BuildDirPathRelease
-	cmake $TestDirPath -G "$Toolset" -DCMAKE_BUILD_TYPE=Release
-	if [ $? -ne 0 ]; then
-		echo "CMake configuration failed for Release build."
-	else
-		GenerateOk=1
-	fi
-fi
-
-BuildOk=0
-if [ $GenerateOk -ne 0 ]; then
-	cd $BuildDirPathDebug
-	cmake --build .
-	if [ $? -ne 0 ]; then
-		echo "CMake build failed for Debug build."
-	else
-		cd $BuildDirPathRelease
-		cmake --build .
-		if [ $? -ne 0 ]; then
-			echo "CMake build failed for Release build."
-		else
-			BuildOk=1
-		fi
-	fi
-fi
-
-if [ $BuildOk -ne 0 ]; then
-	cd $BuildDirPathDebug
 	ctest . --output-on-failure -j 1024
 	if [ $? -ne 0 ]; then
-		echo "CMake test failed for Debug build."
-	else
-		cd $BuildDirPathRelease
-		cmake --build .
-		ctest . --output-on-failure -j 1024
-		if [ $? -ne 0 ]; then
-			echo "CMake test failed for Release build."
-		fi
+		echo "CMake test failed for Release build."
 	fi
 fi
 cd $OldDirPath
--- PlatformSpecificScript/Windows/Test.bat
+++ PlatformSpecificScript/Windows/Test.bat
@@ -1,5 +1,5 @@
 @echo off
-@set BuildDirPath=%cd%\TestBuild
+@set BuildDirPath=%cd%\DefaultBuild
 @set OldDirPath=%cd%
 
 @if defined Toolset (
@@ -8,38 +8,15 @@
 
 @mkdir %BuildDirPath%
 @cd %BuildDirPath%
-@cmake %TestDirPath% %CMakeToolsetArgs%
-@set BuildOk=0
-@if %ERRORLEVEL% neq 0 (
-    echo "### cmake generating failed ###"
+@ctest . --output-on-failure -j 1024 -C Debug
+@if %errorlevel% neq 0 (
+    echo "### test failed ###"
     pause
 ) else (
-    @cmake --build . --config Debug
+    @ctest . --output-on-failure -j 1024 -C Release
     @if %errorlevel% neq 0 (
-        echo "### ctest failed ###"
+        echo "### test failed ###"
         pause
-    ) else (
-        @cmake --build . --config Release
-        @if %errorlevel% neq 0 (
-            echo "### ctest failed ###"
-            pause
-        ) else (
-            @set BuildOk=1
-        )
-    )
-)
-
-@if %BuildOk% neq 0 (
-    @ctest . --output-on-failure -j 1024 -C Debug
-    @if %errorlevel% neq 0 (
-        echo "### ctest failed ###"
-        pause
-    ) else (
-        @ctest . --output-on-failure -j 1024 -C Release
-        @if %errorlevel% neq 0 (
-            echo "### ctest failed ###"
-            pause
-        )
     )
 )
 @cd %OldDirPath%
