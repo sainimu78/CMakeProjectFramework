042eab83856bc6535d98cc0f8aca314915073600
sainimu78 <393584307@qq.com>
2025-04-13 02:20:45 +0800

    修复 bash 错误

--- PlatformSpecificScript/Linux/BuildDebug.sh
+++ PlatformSpecificScript/Linux/BuildDebug.sh
@@ -5,7 +5,7 @@ cd $BuildDirPath
 cmake --build ./Debug -j 1024 --config Debug
 if [ $? -ne 0 ]; then
     echo "CMake configuration failed for Debug build."
-else
+#else
 #	cmake --build ./Release -j 1024 --config Release
 #	if [ $? -ne 0 ]; then
 #		echo "CMake configuration failed for Release build."
