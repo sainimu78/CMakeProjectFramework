12a41559b44ff26d00de2ae357dd1f59bb784f54
sainimu78 <393584307@qq.com>
2025-04-13 01:12:27 +0800

    原逻辑为替换目录, 因此并适用, 回退为 8ae2000493c6038cba6bb971a02a39da0afa4305, 另通过脚本操作解压即可, 正常用法不会对依赖二进制文件版本控制

--- CMakeInclude/InlineDownloadAndUnzip.cmake
+++ CMakeInclude/InlineDownloadAndUnzip.cmake
@@ -1,6 +1,4 @@
 set(DstDownloadedFilePath ${v_ImportedLibRootDirPath}/${v_ZipFileName})
 if(c_ProjectPipelineSetup OR NOT EXISTS "${v_UnzippedDirPath}")
 	download_zip_replace_dir_if_not_exists(${v_SrcAddrZipFilePath} ${DstDownloadedFilePath} ${v_UnzippedDirPath} IsDownloaded)
-elseif(EXISTS "${v_UnzippedDirPath}" AND NOT EXISTS "${v_LibPlatformArchDirPath}")
-	unzip_and_delete($${DstDownloadedFilePath} ${v_UnzippedDirPath})
 endif()
