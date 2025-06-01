de172e3dd0e04e4d645b24636981988e57adf1bb
sainimu78 <393584307@qq.com>
2025-06-01 23:24:38 +0800

    修复 Linux 下运行时链接动态库安装错用

--- CMakeInclude/InstallDynamicLinkinglLibrary.cmake
+++ CMakeInclude/InstallDynamicLinkinglLibrary.cmake
@@ -1,6 +1,12 @@
-install(TARGETS ${ModuleName}
-	RUNTIME DESTINATION "${c_ProjectInstallingTargetDirPath}/${c_BinDirName}"
-)
+if(WIN32)
+	install(TARGETS ${ModuleName}
+		RUNTIME DESTINATION "${c_ProjectInstallingTargetDirPath}/${c_BinDirName}"
+	)
+else()
+	install(TARGETS ${ModuleName}
+		LIBRARY DESTINATION "${c_ProjectInstallingTargetDirPath}/${c_LibDirName}"
+	)
+endif()
 
 if(WIN32)
 	install(FILES "$<TARGET_FILE_DIR:${ModuleName}>/${ModuleName}.pdb"
