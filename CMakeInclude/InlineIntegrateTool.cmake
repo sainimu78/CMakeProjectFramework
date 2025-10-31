5ff27a3194de6ef4b1d145dc845e4ece65af587c
sainimu78 <393584307@qq.com>
2025-10-31 16:07:09 +0000

    revert to 05034c7d46278677ce22990587fdf7a1a2dafb5a

--- CMakeInclude/InlineIntegrateTool.cmake
+++ CMakeInclude/InlineIntegrateTool.cmake
@@ -1,9 +1,7 @@
 if(v_ZipFileName)
 	set(NonPipelineSetupDependencyCheckingDirPath )
 	#指定 v_LibPlatformArchDirPath 可支持 Windows 宿主机中使用 VS + Ubuntu 容器中构建共用挂载目录
-	#但须注意, 否则 Windows 中文件系统修改挂载目录会导致 Ubuntu 的文件系统出错, 出现如头文件中定义重复的编译错误
-	#1. Linux 与 Windows 都可获取到有效包, 应先 Generate.bat 再 Generate.sh 或先 Setup.bat 再 Update.sh
-	#2. 只有 Linux 可获取到有效包, 应先 Generate.sh 再 Generate.bat 或先 Setup.sh 再 Update.bat
+	#但须注意应先 Generate.bat 再 Generate.sh 或先 Setup.bat 再 Update.sh, 否则 Windows 中文件系统修改挂载目录会导致 Ubuntu 的文件系统出错如头文件中定义重复
 	#上述方式, 由于在挂载目录中操作, 构建等过程执行缓慢, 并不建议使用, 更建议使用 VS Code + Remote SSH
 	if(v_LibPlatformArchDirPath)
 		set(NonPipelineSetupDependencyCheckingDirPath ${v_LibPlatformArchDirPath})
